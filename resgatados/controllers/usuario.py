cab1157b1896944bd7b75d1f4c6880b01731da54
Ytalo Ethaniel <ysa4@aluno.ifal.edu.br>
2023-03-14 00:12:25 +0000

    adicionando get & post de /usuario

--- resgatados/controllers/usuario.py
+++ resgatados/controllers/usuario.py
@@ -1,10 +1,45 @@
+import sqlalchemy as sql
+
 from flask_restful import Resource
 from flask import request
+from ..database.db import db
+from sqlalchemy import exc
+
+from ..models.usuario import Usuario as UsuarioModel 
 
 class Usuario(Resource):
     
     def get(self):
-        pass
+        usuarios = db.session.execute(
+            sql.select(UsuarioModel)
+        ).scalars().all()
+        
+        usuarios_json = [u.get_json() for u in usuarios]
+        
+        return usuarios_json
+    
     
     def post(self):
-        print(request)
+        try:
+            req = request.get_json()
+            
+            novo_usuario = UsuarioModel(
+                cpf=req['cpf'],
+                nome=req['nome'],
+                email=req['email'],
+                senha=req['senha'],
+            )
+            if req.get('descricao', -1) != -1:
+                novo_usuario.descricao = req['descricao']
+            if req.get('endereco', -1) != -1:
+                novo_usuario.endereco = req['endereco']
+                
+            db.session.add(novo_usuario)
+            db.session.commit()
+            
+            return novo_usuario.get_json()
+        except KeyError as ke:
+            return { "erro": f"Falta parâmetro obrigatório {str(ke)}" }, 400
+        except exc.IntegrityError:
+            return { "erro": "Usuário já incluso" }, 400
+        
--- resgatados/models/usuario.py
+++ resgatados/models/usuario.py
@@ -2,8 +2,35 @@ from ..database.db import db
 import sqlalchemy as sql
 
 class Usuario(db.Model):
-    cpf = sql.Column(sql.String, primary_key=True)
-    nome = sql.Column(sql.String, nullable=False)
-    email = sql.Column(sql.String, nullable=False)
-    descricao = sql.Column(sql.Text)
-    endereco = sql.Column(sql.Text)
+    cpf = sql.Column(
+        sql.String, 
+        primary_key=True
+    )
+    nome = sql.Column(
+        sql.String, 
+        nullable=False
+    )
+    email = sql.Column(
+        sql.String, 
+        nullable=False
+    )
+    senha = sql.Column(
+        sql.String, 
+        nullable=False
+    )
+    descricao = sql.Column(
+        sql.Text
+    )
+    endereco = sql.Column(
+        sql.Text
+    )
+    
+    def get_json(self):
+        return {
+            "cpf": self.cpf,
+            "nome": self.nome,
+            "email": self.email,
+            "senha": self.senha,
+            "descricao": self.descricao,
+            "endereco": self.endereco
+        }
