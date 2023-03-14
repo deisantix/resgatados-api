ffd9e8686d7563fba0250b7c16aacada7f75efd1
Ytalo Ethaniel <ysa4@aluno.ifal.edu.br>
2023-03-14 18:11:05 +0000

    implementando importacao dinamica de modelos

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
+import os
+from importlib import import_module
+
 from resgatados.config.api import application
 from resgatados.database.db import db
 
 import resgatados.routes.routes
 
-from resgatados.models.usuario import Usuario
+if not os.path.exists('./instance/resgatados.db'):
+    path_of_models = './resgatados/models'
+    models = os.listdir(path_of_models)
+    
+    for file in models:
+        if (file != '__init__.py') and (file != '__pycache__') and (file.endswith('.py')):
+            model_path = os.path.join(path_of_models, file)
+            bare_model_path = model_path[2:-3]
+            model_module = bare_model_path.replace('/', '.')
+            
+            import_module(model_module)
 
 if __name__ == '__main__':
     with application.app_context():
         db.create_all()
     
-    application.run(debug=True, port="3000")
+    application.run(debug=True, port="5050")
